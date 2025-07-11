87aec1068ed65035dbfbbd8d5d3cc57ae36a2809
Arya <109459398+Lordarya1@users.noreply.github.com>
2025-07-11 03:47:31 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from telegram.ext import ApplicationBuilder
 
-TOKEN = os.environ.get("7951728026:AAHe-oylklaqkGxJvaomDuTZNezidMMAUAc")  # توکن از محیط گرفته میشه
+TOKEN = os.environ.get("BOT_TOKEN")  # توکن از محیط گرفته میشه
 
 app = ApplicationBuilder().token(TOKEN).build()
 # ادامه‌ی کدت...
